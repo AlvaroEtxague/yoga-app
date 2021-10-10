f27d830dfe818882550ec51eb708d619bd99a84b
Tinowski <alvaroetxague@gmail.com>
2021-10-10 15:36:55 +0100

    Adding Admin settings for teachers and courses

--- courses/models.py
+++ courses/models.py
@@ -4,17 +4,17 @@ from teachers.models import Teacher
 
 # Create your models here.
 class Course(models.Model):
-    teacher: models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
-    title: models.CharField(max_length=250)
-    lessons: models.IntegerField()
-    description: models.TextField(blank=True)
-    price: models.DecimalField(max_digits=5, decimal_places=2)
-    start_date: models.DateField(default=date.today, blank=True)
-    main_pic: models.ImageField(upload_to='photos/%Y/m/%d/')
-    pic_1: models.ImageField(upload_to='photos/%Y/m/%d/', blank=True)
-    pic_2: models.ImageField(upload_to='photos/%Y/m/%d/', blank=True)
-    pic_3: models.ImageField(upload_to='photos/%Y/m/%d/', blank=True)
-    is_published: models.BooleanField(default=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=250)
+    lessons = models.IntegerField()
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField(default=date.today, blank=True)
+    main_pic = models.ImageField(upload_to='photos/%Y/m/%d/')
+    pic_1 = models.ImageField(upload_to='photos/%Y/m/%d/', blank=True)
+    pic_2 = models.ImageField(upload_to='photos/%Y/m/%d/', blank=True)
+    pic_3 = models.ImageField(upload_to='photos/%Y/m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
     
     def __str__(self):
         return self.title
--- yoga_app/settings.py
+++ yoga_app/settings.py
@@ -67,9 +67,18 @@ WSGI_APPLICATION = 'yoga_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yogaapp',
+        'USER': 'postgres',
+        'PASSWORD': 'password1',
+        'HOST': 'localhost',
+        'PORT': '9001',
     }
 }
 
