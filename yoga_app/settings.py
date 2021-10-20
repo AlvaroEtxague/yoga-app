fb266b92f9a1cb5bfb18e523b182ceaa8af8dfc8
Tinowski <alvaroetxague@gmail.com>
2021-10-20 21:35:34 +0100

    Removing secret key

--- yoga_app/settings.py
+++ yoga_app/settings.py
@@ -10,9 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-r6)4mr_1=o!ysuw@h&98%4()%2uxw(-f20nt7+v1@7zq3j0h-9"
-)
+SECRET_KEY = "<<ADD_SECRET_KEY_HERE>>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
