bded71764c1f27cee48456b25ae0e4ce54becf9a
Tinowski <alvaroetxague@gmail.com>
2021-10-22 13:07:04 +0100

    Installed whitenoise

--- yoga_app/settings.py
+++ yoga_app/settings.py
@@ -46,6 +46,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "yoga_app.urls"
