e16898192303190afb33acf8da51a6c763d7c89f
Tinowski <alvaroetxague@gmail.com>
2021-10-22 20:23:08 +0100

    Adding Stripe payments core functionality

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.courses, name='courses'),
     path('<int:course_id>', views.course, name='course'),
+    path('checkout/', views.CheckoutSession.as_view(), name='checkout'),
+    path('checkout-success/', views.CheckoutSuccess.as_view(),
+         name='checkout_success'),
+    path('checkout-cancel/', views.CheckoutCancel.as_view(), name='checkout_cancel'),
+    path('webhook/', views.stripe_webhook, name="stripe_webhook")
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import get_object_or_404, render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
 from .models import Course
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import View, TemplateView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.conf import settings
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
 
@@ -26,3 +32,76 @@ def course(request, course_id):
         'course': course
     }
     return render(request, 'courses/course.html', context)
+
+
+class CheckoutSession(View):
+    def post(self, request, *args, **kwargs):
+        course = get_object_or_404(Course, id=request.POST.get('course'))
+
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+
+                        'name': course.title,
+                        'description': course.description,
+                        'quantity': 1,
+                        'currency': 'EUR',
+                        'amount':  int(course.price * 100),
+                    }
+                ],
+                payment_method_types=[
+                    'card',
+                ],
+                mode='payment',
+                success_url=request.build_absolute_uri(
+                    reverse('checkout_success')),
+                cancel_url=request.build_absolute_uri(
+                    reverse('checkout_cancel'))
+            )
+        except Exception as e:
+            print(e)
+            return str(e)
+        return HttpResponseRedirect(checkout_session.url)
+
+
+class CheckoutSuccess(TemplateView):
+    template_name = "courses/checkout_success.html"
+
+
+class CheckoutCancel(TemplateView):
+    template_name = "courses/checkout_success.html"
+
+
+def fulfill_order(session):
+    # TODO: fill me in
+    print("Fulfilling order", session)
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    print("#"*20)
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fulfill the purchase...
+        fulfill_order(session)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
