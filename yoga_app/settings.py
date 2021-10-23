5943175c75547900ec02c0eb7f0b9f0cc90604e0
Tinowski <alvaroetxague@gmail.com>
2021-10-23 13:15:04 +0100

    Updating region

--- yoga_app/settings.py
+++ yoga_app/settings.py
@@ -160,7 +160,7 @@ if "USE_S3" in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'EU (Ireland) eu-west-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # s3 static and media settings
